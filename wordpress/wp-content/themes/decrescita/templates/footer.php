db56a37fbe7fcc76da04186931e0f6b017ccdbc5
kiarapaleari <kiarapaleari@gmail.com>
2014-12-12 18:05:01 +0100

    footer

--- wordpress/wp-content/themes/decrescita/templates/footer.php
+++ wordpress/wp-content/themes/decrescita/templates/footer.php
@@ -1,4 +1,11 @@
 <footer class="content-info row" role="contentinfo">
-	<?php dynamic_sidebar('text-footer'); ?>
-	<?php dynamic_sidebar('sidebar-footer'); ?>
+	<div class="col-md-6">
+		<?php dynamic_sidebar('text-footer'); ?>
+	</div>
+	<div class="col-md-3">
+		<?php dynamic_sidebar('sidebar-footer'); ?>
+	</div>
+	<div class="col-md-3">
+		<?php dynamic_sidebar('sidebar-footer'); ?>
+	</div>
 </footer>
