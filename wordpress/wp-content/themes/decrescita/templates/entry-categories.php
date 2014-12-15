01a648a07d1439d26f64b0b3ab18ca954db1385a
ipalo <rosso.ipalo@gmail.com>
2014-12-15 11:59:45 +0100

    sistemato layout homepage, altre modifiche minori

--- wordpress/wp-content/themes/decrescita/templates/entry-categories.php
+++ wordpress/wp-content/themes/decrescita/templates/entry-categories.php
@@ -1,7 +1,8 @@
 <p class="post categories">
 	<?php
-	if(!empty(get_the_category())) {
-		foreach(get_the_category() as $category) {
+	$categories = get_the_category();
+	if(!empty($categories)) {
+		foreach($categories as $category) {
 			echo '<a class="label label-categoria" href="'.get_category_link($category->term_id).'" title="'.esc_attr(sprintf(__( "View all posts in %s", 'decrescita'), $category->name)).'">'.$category->cat_name.'</a> ';
 		}
 	}?>
--- wordpress/wp-content/themes/decrescita/templates/entry-temi.php
+++ wordpress/wp-content/themes/decrescita/templates/entry-temi.php
@@ -1,7 +1,8 @@
 <p class="byline author vcard temi">
 	<?php
-	if(!empty(get_the_category())) {
-		foreach(get_the_terms($post->ID, 'temi') as $term) {
+	$terms = get_the_terms($post->ID, 'temi');
+	if(!empty($terms)) {
+		foreach($terms as $term) {
 			echo '<a class="label label-categoria" href="'.get_term_link($term->slug, 'temi').'" title="'.esc_attr(sprintf(__( "View all posts in %s", 'decrescita'), $term->name)).'">'.$term->name.'</a> ';
 		}
 	}?>
