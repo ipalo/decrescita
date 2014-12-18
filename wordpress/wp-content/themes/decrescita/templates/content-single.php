8b171faaa42ee3d19bf531476c441a06b56dca16
kiarapaleari <kiarapaleari@gmail.com>
2014-12-18 14:35:07 +0100

    single article

--- wordpress/wp-content/themes/decrescita/templates/content-single.php
+++ wordpress/wp-content/themes/decrescita/templates/content-single.php
@@ -1,10 +1,20 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
+      <p class="post categories">
+        <?php
+        $categories = get_the_category();
+        if(!empty($categories)) {
+        foreach($categories as $category) {
+        echo '<a class="label label-categoria" href="'.get_category_link($category->term_id).'" title="'.esc_attr(sprintf(__( "View all posts in %s", 'decrescita'), $category->name)).'">'.$category->cat_name.'</a> ';
+        }
+      }?>
+      </p>
+      <h2 class="entry-title"><?php the_title(); ?></h2>
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
     <div class="entry-content">
+      <?php the_post_thumbnail(); ?>
       <?php the_content(); ?>
     </div>
     <footer>
