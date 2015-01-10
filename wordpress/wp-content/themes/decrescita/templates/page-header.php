26187acfd79630328467ce59b256c7addf876bab
ipalo <rosso.ipalo@gmail.com>
2015-01-10 17:23:16 +0100

    risolto errore kia

--- wordpress/wp-content/themes/decrescita/templates/page-header.php
+++ wordpress/wp-content/themes/decrescita/templates/page-header.php
@@ -3,7 +3,8 @@
 	  <h1>
 	    <?php echo roots_title(); ?>
 	  </h1>
-	  <?php if((is_category() OR is_tag() OR is_tax()) AND !empty(term_description())) : ?>
+	  <?php $desc = term_description(); ?>
+	  <?php if((is_category() OR is_tag() OR is_tax()) AND !empty($desc)) : ?>
 	  	<div id="taxonomy_description">
 	  		<?php echo do_shortcode(term_description()); ?>
 	  	</div>
