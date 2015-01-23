c1b5d14c0d93736cbc280d9a5d66b6a962b031e4
ipalo <rosso.ipalo@gmail.com>
2015-01-23 17:29:45 +0100

    modificato article per format-quote

--- wordpress/wp-content/themes/decrescita/templates/entry-persone.php
+++ wordpress/wp-content/themes/decrescita/templates/entry-persone.php
@@ -3,7 +3,7 @@
 	$terms = get_the_terms($post->ID, 'persone');
 	if(!empty($terms)) {
 		foreach($terms as $term) {
-			echo '<a class="label label-categoria" href="'.get_term_link($term->slug, 'persone').'" title="'.esc_attr(sprintf(__( "Tutti gli articoli in %s", 'decrescita'), $term->name)).'">'.$term->name.'</a> ';
+			echo '<a class="label label-persone" href="'.get_term_link($term->slug, 'persone').'" title="'.esc_attr(sprintf(__( "Tutti gli articoli in %s", 'decrescita'), $term->name)).'">'.$term->name.'</a> ';
 		}
 	}?>
 </p>
